cf570509859255e89ed4a76cd2dc0b57b420f7d4
Kirill Ylianov <85870150+DaiPoigrat@users.noreply.github.com>
2024-04-13 21:51:25 +0400

    Update task02.ino

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,94 +1,91 @@
 #include <TaskManagerIO.h>
- 
-const int led_pin = 3;
-const int led_pin2 = 5;
-const int led_pin3 = 6;
-const int led_pin4 = 9;
-const int led_pin5 = 10;
 
-
-int led_state = LOW; 
-int led_state2 = LOW;
-int led_state3 = LOW;
-int led_state4 = LOW;
-int led_state5 = LOW;
-
-
-unsigned long previous_millis = 0; 
-unsigned long previous_millis2 = 0; 
-unsigned long previous_millis3 = 0; 
-unsigned long previous_millis4 = 0; 
-unsigned long previous_millis5 = 0;  
-
-
-const long interval = 10000;
-const long interval2 = 1000;
-const long interval3 = 500;
-const long interval4 = 100;
-const long interval5 = 50;  
-
-void blink()
-{
-  if (led_state == LOW) {
-      led_state = HIGH;
-    } else {
-      led_state = LOW;
-    }
-  digitalWrite(led_pin, led_state);
-}
-void blink2()
-{
-  if (led_state2 == LOW) {
-      led_state2 = HIGH;
-    } else {
-      led_state2 = LOW;
-    }
-  digitalWrite(led_pin2, led_state2);
-}
-void blink3()
-{
-  if (led_state3 == LOW) {
-      led_state3 = HIGH;
-    } else {
-      led_state3 = LOW;
-    }
-  digitalWrite(led_pin3, led_state3);
-}
-void blink4()
-{
-  if (led_state4 == LOW) {
-      led_state4 = HIGH;
-    } else {
-      led_state4 = LOW;
-    }
-  digitalWrite(led_pin4, led_state4);
-}
-void blink5()
-{
-  if (led_state5 == LOW) {
-      led_state5 = HIGH;
-    } else {
-      led_state5 = LOW;
-    }
-  digitalWrite(led_pin5, led_state5);
-}
-
+long times[5] = {10000, 1000, 500, 100, 50}; //10мс 1мс 500мкс 100мкс 50мкс
+int* state = new int[5];
+int pins[5] = {3, 5, 6, 9, 10};
 
 void setup() {
+  // put your setup code here, to run once:
+  for (int i = 0; i < 5; i++) 
+  {
+    state[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+  }
+  // Create a task that's scheduled every times[0]
+  taskid_t task0 = taskManager.scheduleFixedRate(times[0], [] {
+      if (state[0] == LOW) 
+      {
+        state[0] = HIGH;
+      }
+      else {
+        state[0] = LOW;
+      }
 
-  pinMode(led_pin, OUTPUT);
-  pinMode(led_pin2, OUTPUT);
-  pinMode(led_pin3, OUTPUT);
-  pinMode(led_pin4, OUTPUT);
-  pinMode(led_pin5, OUTPUT);
-  
-  taskManager.schedule(repeatMicros(interval), blink);
-  taskManager.schedule(repeatMicros(interval2), blink2);
-  taskManager.schedule(repeatMicros(interval3), blink3);
-  taskManager.schedule(repeatMicros(interval4), blink4);
-  taskManager.schedule(repeatMicros(interval5), blink5);
+      digitalWrite(pins[0], state[0]);
+    }, TIME_MICROS);
+
+  taskid_t task1 = taskManager.scheduleFixedRate(times[1], [] {
+      if (state[1] == LOW) 
+      {
+        state[1] = HIGH;
+      }
+      else {
+        state[1] = LOW;
+      }
+
+      digitalWrite(pins[1], state[1]);
+    }, TIME_MICROS);
+
+  taskid_t taskId2 = taskManager.scheduleFixedRate(times[2], [] {
+      if (state[2] == LOW) 
+      {
+        state[2] = HIGH;
+      }
+      else {
+        state[2] = LOW;
+      }
+
+      digitalWrite(pins[2], state[2]);
+    }, TIME_MICROS);
+
+  taskid_t task3 = taskManager.scheduleFixedRate(times[3], [] {
+      if (state[3] == LOW) 
+      {
+        state[3] = HIGH;
+      }
+      else {
+        state[3] = LOW;
+      }
+
+      digitalWrite(pins[3], state[3]);
+    }, TIME_MICROS);
+
+  taskid_t task4 = taskManager.scheduleFixedRate(times[4], [] {
+      if (state[4] == LOW) 
+      {
+        state[4] = HIGH;
+      }
+      else {
+        state[4] = LOW;
+      }
+
+      digitalWrite(pins[4], state[4]);
+    }, TIME_MICROS);
+
+  taskid_t task5 = taskManager.scheduleFixedRate(times[5], [] {
+      if (state[5] == LOW) 
+      {
+        state[5] = HIGH;
+      }
+      else {
+        state[5] = LOW;
+      }
+
+      digitalWrite(pins[5], state[5]);
+    }, TIME_MICROS);
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   taskManager.runLoop();
 }
